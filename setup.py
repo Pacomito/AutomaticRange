41f416082b18555686504b66723d595921b96f6d
Pacome Prompsy <pacome.pr@gmail.com>
2025-07-15 17:02:34 +0200

    Changed Package name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='napari-automatic-range',
+    name='AutomaticRange',
     version='0.1.0',
     author='Pacôme Prompsy',
     author_email='pacome.prompsy@unil.ch',
-    description='A Napari plugin for automatic range prediction.',
+    description='A python package for automatic range prediction.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -15,9 +15,4 @@ setup(
         'scipy',
         'scikit-image'
     ],
-    entry_points={
-    'napari.plugin': [
-        'napari-automatic-range = napari_automatic_range',
-    ],
-},
 )
