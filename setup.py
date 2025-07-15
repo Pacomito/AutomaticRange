ddd7257f8202c52497a7319e9b6a2f378fc858b3
Pacome Prompsy <pacome.pr@gmail.com>
2025-07-15 17:51:09 +0200

    Imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AutomaticRange',
-    version='0.1.2',
+    version='0.1.3',
     author='Pacôme Prompsy',
     author_email='pacome.prompsy@unil.ch',
     description='A python package for automatic range prediction.',
